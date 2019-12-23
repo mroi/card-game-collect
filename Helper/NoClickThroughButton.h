4754e8be9dbd21e0b84ac39cf93cc7f38b0395d6
Michael Roitzsch <reactorcontrol@icloud.com>
2019-12-23 21:40:43 +0100

    remove current project to start fresh in Swift

--- Helper/NoClickThroughButton.h
+++
@@ -1,5 +0,0 @@
-/* Implements a NSButton that does not support click-through, when the window is not active. */
-/* There should be a way in Cocoa to do this without subclassing, but there is none. */
-
-@interface NoClickThroughButton : NSButton
-@end
