b63b9829603bbebe71f2ef0045ccf231a4250157
Michael Roitzsch <reactorcontrol@icloud.com>
2020-11-01 15:26:02 +0100

    initial Swift project

---
+++ View/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  CardGameCollect
+//
+//  Created by Michael Roitzsch on 01.11.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
