d6ace8c15f0c2f23ca8dd815fb8a1cdab2ca3e5f
Michael Roitzsch <reactorcontrol@icloud.com>
2021-04-04 18:10:29 +0200

    update to modern SwiftUI app structure

--- View/ContentView.swift
+++ View/ContentView.swift
@@ -1,22 +1,31 @@
-//
-//  ContentView.swift
-//  CardGameCollect
-//
-//  Created by Michael Roitzsch on 01.11.20.
-//
-
 import SwiftUI
 
+// FIXME: remove when we use better test data
+extension String: Identifiable {
+	public var id: String {
+		return self
+	}
+}
+
 struct ContentView: View {
+
+	// FIXME: test data, fetch from database
+	var cardGames: [String] = ["Hello", "World"]
+
     var body: some View {
-        Text("Hello, World!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+		NavigationView {
+			List(cardGames) { cardGame in
+				Text(cardGame)
+			}
+			Text("Hello, World!")
+				.frame(maxWidth: .infinity, maxHeight: .infinity)
+		}
     }
 }
 
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
 }
